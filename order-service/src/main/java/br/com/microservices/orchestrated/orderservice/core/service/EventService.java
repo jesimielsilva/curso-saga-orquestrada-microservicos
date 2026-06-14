ca0a217e6ef992d2e2a476ef3a0bc3ff55e065af
Jesimiel <jesimielbarbosa@gmail.com>
2026-06-14 16:53:43 -0300

    feat: Order-service, Criando Endpoint post e get...

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,12 +1,17 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
+import br.com.microservices.orchestrated.orderservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.dto.EventFilters;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +25,34 @@ public class EventService {
         event.setCreatedAt(LocalDateTime.now());
         save(event);
         log.info("Order {} Event with transactionId {}", event.getOrderId(), event.getTransactionId());
+    }
+
+    public List<Event> findAll() {
+        return repository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public Event findByFilter(EventFilters filters) {
+        if (!isEmpty(filters.getOrderId())) {
+            return findByOrderId(filters.getOrderId());
+        } else {
+            return findByTransactionId(filters.getTransactionId());
+        }
+    }
+
+    private Event findByOrderId(String orderId) {
+        return repository.findTop1ByOrderIdOrderByCreatedAtDesc(orderId)
+                .orElseThrow(() -> new ValidationException(String.format("No event found for orderId %s", orderId)));
+    }
+
+    private Event findByTransactionId(String transactionId) {
+        return repository.findTop1ByTransactionIdOrderByCreatedAtDesc(transactionId)
+                .orElseThrow(() -> new ValidationException(String.format("No event found for transactionId %s", transactionId)));
+    }
+
+    private void validateEmptyFilters(EventFilters eventFilters) {
+        if (isEmpty(eventFilters.getOrderId()) && isEmpty(eventFilters.getTransactionId())) {
+            throw new ValidationException("OrderID or TransactionID must be informed");
+        }
     }
 
     public Event save(Event event) {
